fc202b5951f18978c3a532ea444a80d60a8c6154
Mohit <mohitpawaskar33@gmail.com>
2025-11-04 21:25:47 +0530

    Started Java from Scratch

---
+++ Java/02_Variables.java
@@ -0,0 +1,13 @@
+class Variables {
+    public static void main(String[] args) {
+        String name = "Mohit";
+        String surname = "Pawaskar";
+        int age = 23;
+        float cgpa = 6.86f;
+        
+        
+        System.out.println("Name:\t"+name+surname);
+        System.out.println("Age:\t"+age);
+        System.out.println("CGPA:\t"+cgpa);
+    }
+}
---
+++ Java/03_DataTypes.java
@@ -0,0 +1,28 @@
+class DataTypes {
+    public static void main(String[] args) {
+        /* byte - 1 [-128 to 127]
+        short - 2
+        int - 4
+        float - 4
+        double - 8
+        long - 8
+        char - 2
+        boolean - 1
+j
+        */
+
+        // Primtive Types
+        // byte age = 23;
+        // int phone = 852642632;
+        // long Phone = 154941345416413L;
+        // float PI = 3.14F;
+        // char ch = 'a';
+        // boolean isAdult = true;
+        
+
+        // Non-Primtive Types
+        String name = "Mohit";
+        System.out.println("The Length of Name is: "+name.length());
+
+    }
+}
